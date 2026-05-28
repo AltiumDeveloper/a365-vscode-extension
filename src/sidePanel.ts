75bc1c698d9434f8202dd688cfce376aa29f868c
Dmitry Kolomiets <dmitry.kolomiets@altium.com>
2026-05-28 01:17:33 +0100

    feat(10): dim workflow assignments to indicate not-yet-supported status
    Apply descriptionForeground theme color to workflow assignment icons to
    visually indicate they are not currently actionable (unlike script
    assignments which can be edited/run).
    
    This provides a subtle visual cue that workflows are informational only
    in the current implementation.

--- src/sidePanel.ts
+++ src/sidePanel.ts
@@ -270,7 +270,10 @@ export class A365TreeDataProvider implements vscode.TreeDataProvider<A365Node> {
                         arguments: [n],
                     };
                 } else if (n.assignment.type === 'WORKFLOW') {
-                    item.iconPath = new vscode.ThemeIcon('git-pull-request');
+                    item.iconPath = new vscode.ThemeIcon(
+                        'git-pull-request',
+                        new vscode.ThemeColor('descriptionForeground')
+                    );
                     item.contextValue = CTX_ASSIGNMENT_WORKFLOW;
                 } else if (n.assignment.type === 'DEFAULT') {
                     item.iconPath = new vscode.ThemeIcon(
